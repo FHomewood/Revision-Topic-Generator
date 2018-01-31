e18c5dc2b84773aab884b4456830b46a762e3cb5
F.Homewood <fhomewood98@gmail.com>
2018-01-31 18:09:39 +0000

    First draft including exception handling

---
+++ RevTG/RevTG/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RevTG
+{
+    partial class RevTG_Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txbDay = new System.Windows.Forms.TextBox();
+            this.txbMonth = new System.Windows.Forms.TextBox();
+            this.rbOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(143, 9);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(53, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txbDay
+            // 
+            this.txbDay.AccessibleDescription = "";
+            this.txbDay.Location = new System.Drawing.Point(13, 9);
+            this.txbDay.Name = "txbDay";
+            this.txbDay.Size = new System.Drawing.Size(59, 20);
+            this.txbDay.TabIndex = 1;
+            this.txbDay.Tag = "";
+            // 
+            // txbMonth
+            // 
+            this.txbMonth.Location = new System.Drawing.Point(78, 9);
+            this.txbMonth.Name = "txbMonth";
+            this.txbMonth.Size = new System.Drawing.Size(59, 20);
+            this.txbMonth.TabIndex = 2;
+            // 
+            // rbOutput
+            // 
+            this.rbOutput.BackColor = System.Drawing.SystemColors.Control;
+            this.rbOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rbOutput.Location = new System.Drawing.Point(13, 39);
+            this.rbOutput.Name = "rbOutput";
+            this.rbOutput.Size = new System.Drawing.Size(183, 226);
+            this.rbOutput.TabIndex = 3;
+            this.rbOutput.Text = "";
+            // 
+            // RevTG_Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(211, 277);
+            this.Controls.Add(this.rbOutput);
+            this.Controls.Add(this.txbMonth);
+            this.Controls.Add(this.txbDay);
+            this.Controls.Add(this.btnFind);
+            this.Name = "RevTG_Form1";
+            this.Text = "RevTG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txbDay;
+        private System.Windows.Forms.TextBox txbMonth;
+        private System.Windows.Forms.RichTextBox rbOutput;
+    }
+}
+
---
+++ RevTG/RevTG/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevTG
+{
+    public partial class RevTG_Form1 : Form
+    {
+        List<DateTime> LecDates = new List<DateTime>();
+        List<String> LecTitle = new List<String>();
+
+
+        public RevTG_Form1()
+        {
+            InitializeComponent();
+            txbDay.Text = DateTime.Today.Day.ToString();
+            txbMonth.Text = DateTime.Today.Month.ToString();
+
+
+
+
+            //Week 1
+            LecDates.Add(new DateTime(2018, 02, 5));
+            LecTitle.Add("Thermal and Statistical Physics");
+            LecDates.Add(new DateTime(2018, 02, 5));
+            LecTitle.Add("Theoretical Physics");
+            LecDates.Add(new DateTime(2018, 02, 6));
+            LecTitle.Add("Thermal and Statistical Physics");
+            LecDates.Add(new DateTime(2018, 02, 6));
+            LecTitle.Add("Skills in Physics II");
+            LecDates.Add(new DateTime(2018, 02, 6));
+            LecTitle.Add("Quantum Mechanics I");
+            LecDates.Add(new DateTime(2018, 02, 7));
+            LecTitle.Add("Theoretical Physcis");
+            LecDates.Add(new DateTime(2018, 02, 7));
+            LecTitle.Add("Skills in Physics II");
+            LecDates.Add(new DateTime(2018, 02, 9));
+            LecTitle.Add("Quantum Mechanics I");
+            LecDates.Add(new DateTime(2018, 02, 9));
+            LecTitle.Add("Theoretical Physcis");
+            //Week 2 - 11
+            foreach (int k in new int[] { 0, 1, 2, 3, 4, 5, 8, 9, 10, 11 })
+            {
+                LecDates.Add(new DateTime(2018, 02, 12).AddDays(7 * k));
+                LecTitle.Add("Thermal and Statistical Physics");
+                LecDates.Add(new DateTime(2018, 02, 12).AddDays(7 * k));
+                LecTitle.Add("Theoretical Physcis");
+                LecDates.Add(new DateTime(2018, 02, 13).AddDays(7 * k));
+                LecTitle.Add("Thermal and Statistical Physics");
+                LecDates.Add(new DateTime(2018, 02, 13).AddDays(7 * k));
+                LecTitle.Add("Skills in Physics II");
+                LecDates.Add(new DateTime(2018, 02, 13).AddDays(7 * k));
+                LecTitle.Add("Quantum Mechanics I");
+                LecDates.Add(new DateTime(2018, 02, 14).AddDays(7 * k));
+                LecTitle.Add("Theoretical Physcis");
+                LecDates.Add(new DateTime(2018, 02, 14).AddDays(7 * k));
+                LecTitle.Add("Skills in Physics II");
+                LecDates.Add(new DateTime(2018, 02, 15).AddDays(7 * k));
+                LecTitle.Add("Theoretical Physcis");
+                LecDates.Add(new DateTime(2018, 02, 16).AddDays(7 * k));
+                LecTitle.Add("Theoretical Physcis");
+                LecDates.Add(new DateTime(2018, 02, 16).AddDays(7 * k));
+                LecTitle.Add("Quantum Mechanics I");
+            }
+            //Week 12
+            LecDates.Add(new DateTime(2018, 05, 8));
+            LecTitle.Add("Thermal and Statistical Physics");
+            LecDates.Add(new DateTime(2018, 05, 8));
+            LecTitle.Add("Skills in Physics II");
+            LecDates.Add(new DateTime(2018, 05, 8));
+            LecTitle.Add("Quantum Mechanics I");
+            LecDates.Add(new DateTime(2018, 05, 9));
+            LecTitle.Add("Theoretical Physcis");
+            LecDates.Add(new DateTime(2018, 05, 9));
+            LecTitle.Add("Skills in Physics II");
+            LecDates.Add(new DateTime(2018, 05, 10));
+            LecTitle.Add("Theoretical Physcis");
+            LecDates.Add(new DateTime(2018, 05, 11));
+            LecTitle.Add("Theoretical Physcis");
+            LecDates.Add(new DateTime(2018, 05, 11));
+            LecTitle.Add("Quantum Mechanics I");
+        }
+
+        public void btnFind_Click(object sender, EventArgs e)
+        {
+            rbOutput.Text = "";
+            DateTime RequestDate = DateTime.Today;
+            bool validDate = true;
+            try { RequestDate = new DateTime(2018, int.Parse(txbMonth.Text), int.Parse(txbDay.Text)); }
+            catch { validDate = false; }
+
+            if (validDate)
+                for (int i = 0; i < LecTitle.Count(); i++)
+                {
+                    foreach (int k in new int[] { 2, 6, 14, 30, 62 }) //this is the list of days you will be reminded about the lecture
+                    {
+                        if ((RequestDate - LecDates[i]).Days == k)
+                        {
+                            rbOutput.Text += LecTitle[i] + "\n" + LecDates[i].ToShortDateString() + "\n" + k.ToString() + " Days. \n\r";
+                        }
+                    }
+                }
+            else rbOutput.Text += "That date doesn't exist please input a real date <day> <month>";
+            if (rbOutput.Text == "") rbOutput.Text += "No work scheduled for today :)";
+        }
+    }
+}
